fcba1f5f97252e77ff8e1fd92ec7946893ff1141
Har23s <158301370+Har23s@users.noreply.github.com>
2025-12-29 14:11:15 +0530

    Update 01_bronze_to_silver.py

--- code/04_Medallion_Transformation/01_bronze_to_silver.py
+++ code/04_Medallion_Transformation/01_bronze_to_silver.py
@@ -2,7 +2,7 @@ from pyspark.sql.functions import (
     col, to_date, date_format, trim, initcap,
     split, size, when, concat, lit, abs, to_timestamp, regexp_extract
 )
-import dlt
+import dlt #delta live 
 
 # Configuration of the schema 
 catalog = "insurance_claim_project"
